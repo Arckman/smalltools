3884568b48666d3a13f52909f0e299c74d659a60
Arckman <fengrj1989@gmail.com>
2015-10-22 20:55:51 +0800

    logfilter 	usage: logfilter -f keywordfile logfile 	usage: logfilter [keyword list] logfile

--- logfilter.sh
+++ logfilter.sh
@@ -1,23 +1,47 @@
 #/bin/bash
 name=`basename $0`
+usage="Usage: $name [keyword list] logfile"
+usagef="Usage: $name -f KeywordFile logfie"
 if [ $# -lt 2 ] ;then
-	echo Usage: $name [keyword list] sourcefile
+	echo $usage
+	echo $usagef
 	exit 1
 fi
 if [ -f ${!#} ];then	#can not use ${$#}
 	#echo Processing...
-	#generate output file
+	#generate keywordlist and script
 	output=''
 	script=''
-	for (( i=1;i<$#;i++ )); do
-		output=$output${!i}
-		script=$script\/${!i}\/p\;
-		script=$script\/${!i}\/b\;
-	done
+	keywordlist=()
+	case $1 in
+	-f)
+		if [ $# -ne 3 ] ;then
+			echo $usagef
+			exit 1
+		fi
+		if [ ! -f $2 ] ;then
+			echo "file [$2] can not found!"
+			exit 1
+		fi
+		while read word #keywordlist kept unchanged when using pipe
+		do
+			keywordlist[${#keywordlist[@]}]=${word}
+		done < $2 ;;
+	*)
+		#echo $@
+		keywordlist=( $@ )
+		unset keywordlist[${#keywordlist[@]}-1];;
+	esac
 	#echo $output
-	#echo $script
+	#echo ${keywordlist[*]}
+	for (( i=0;i<${#keywordlist[@]};i++ )); do
+		#output=$output${!i}
+		script=$script\/${keywordlist[i]}\/p\;
+		script=$script\/${keywordlist[i]}\/b\;
+	done
 	script=\{$script\}
-	sed -n -e $script ${!#}
+	#echo $script
+	sed -n -e "$script" ${!#}
 else
 	echo $2 not found!
 fi
